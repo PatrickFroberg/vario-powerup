17a5695a6630dedbd3e0c788df1c84f9ec6df460
Patrick Hou Froberg <patrickfroberg@hotmail.dk>
2022-10-27 14:47:20 +0200

    clean up

---
+++ Vario/Assets/Scripts/TrophyBehavior.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrophyBehavior : MonoBehaviour
+{
+    public GameObject CollecEffect;
+    public Text WinnerText;
+
+    private void Start()
+    {
+        WinnerText.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+            Instantiate(CollecEffect, transform.position, transform.rotation);
+            collision.gameObject.GetComponent<PlayerController>().enabled = false;
+
+            WinnerText.enabled = true;
+        }
+    }
+}
