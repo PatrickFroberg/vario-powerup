a417df821b426ca5176c421b2e88997e3dd9a99b
Patrick Hou Froberg <Patrickfroberg@hotmail.dk>
2022-10-31 18:13:51 +0100

    Adds DieZone

---
+++ Vario/Assets/Scripts/DieZone.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DieZone : MonoBehaviour
+{
+    public GameObject DieEffect;
+    public float TimeUntilRestart = 3f;
+    public Text DieText;
+
+    private void Start()
+    {
+        DieText.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            StartCoroutine(Collect(collision));
+    }
+
+    private IEnumerator Collect(Collider2D player)
+    {
+        Instantiate(DieEffect, player.transform.position, player.transform.rotation);
+
+        player.gameObject.GetComponent<PlayerController>().enabled = false;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        DieText.enabled = true;
+
+        Debug.Log("Dead");
+
+        yield return new WaitForSeconds(TimeUntilRestart);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
